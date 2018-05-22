2f68accd1c63964d9e693b101950df3802efe6a4
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2018-05-22 14:02:27 -0400

    Fix Python adult reader.

--- Python/LogRegression/LogRegression.py
+++ Python/LogRegression/LogRegression.py
@@ -1,5 +1,27 @@
-#imports DPrivacy tools and loads datasets
-exec(open('../LoadData.py').read())
+import sys
+import os
+sys.path.append(os.path.abspath("../../datasets/adult"))
+import adult as adult
+
+from sklearn import linear_model, model_selection
+import pandas as pd
+import numpy as np
+
+original_data = adult.original
+X = adult.binary.fillna(value = 0.0)
+y = adult.labels.fillna(value = 0.0)
+indices = range(0, len(X))
+
+X_train, X_test, y_train, y_test, indices_train, indices_test = model_selection.train_test_split(X, y, indices, train_size=0.70)
+
+logit1 = linear_model.LogisticRegression(C = 1.0)
+logit1 = logit1.fit(X_train, y_train)
+
+logit2 = linear_model.LogisticRegression(C = 1.5)
+logit2 = logit2.fit(X_train, y_train)
+
+logit3 = linear_model.LogisticRegression(C = 0.5)
+logit3 = logit3.fit(X_train, y_train)
 
 class LogRegressionChooser:
     def __init__(hyperparams, mfeatures, train_dbs, scorefunc):
--- datasets/adult/adult.py
+++ datasets/adult/adult.py
@@ -11,20 +11,27 @@ path = os.path.dirname(os.path.realpath(__file__))
 train_path = os.path.join(path, 'adult.data')
 test_path = os.path.join(path, 'adult.test')
 
-original = pd.read_csv(train_path, names = column_names, sep = r'\s*,\s*', engine = 'python', na_values = '?')
-original_test = pd.read_csv(test_path, names = column_names, sep = r'\s*,\s*', engine = 'python', na_values = '?')
+original_train = pd.read_csv(train_path,
+                             names = column_names,
+                             sep = r'\s*,\s*',
+                             engine = 'python',
+                             na_values = '?',
+                             true_values = ['>50K'],
+                             false_values = ['<=50K'])
+original_test = pd.read_csv(test_path,
+                            names = column_names,
+                            comment = '|',
+                            sep = r'\s*,\s*',
+                            engine = 'python',
+                            na_values = '?',
+                            true_values = ['>50K.'],
+                            false_values = ['<=50K.'])
 
-original = pd.concat([original, original_test])
+original = pd.concat([original_train, original_test], ignore_index=True)
 
 del original['fnlwgt']
 del original["Education"]
 
 binary = pd.get_dummies(original)
-
-# Let's fix the Target as it will be converted to dummy vars too
-binary["Target"] = binary["Target_>50K"]
-del binary["Target_<=50K"]
-del binary["Target_>50K"]
-
 labels = binary["Target"]
 binary = binary[binary.columns.difference(["Target"])]
