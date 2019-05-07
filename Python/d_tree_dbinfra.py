ad3dafef1ea0fb2196f9c8e1f2db26068d2a3f62
Jacob Imola <jimolaaa@gmail.com>
2019-05-07 15:17:37 -0700

    Added letter recognition dataset

---
+++ Python/d_tree_dbinfra.py
@@ -0,0 +1,71 @@
+
+
+class null_cm:
+    def __init__(self):
+        self.leaf = Leaf()
+        self.split = Split()
+    def choose(self, db):
+        if db.depth < db.max_depth:
+            return self.split.run(db)
+        else:
+            return self.leaf.run(db)
+
+"""
+Performs dataset surgery on a seed_db. Parameters such as target number of rows are fixed here, 
+which makes sense because seed databases are roughly the same size in this experiment.
+
+Parameters:
+seed_db: dataset to slice up
+eps: value of epsilon for the experiments
+prng: random number generator
+returns a tuple: (regret of algorithm on each db, metafeatures associated with db, and the db itself)
+"""
+def get_train_dbs(seed_db, eps, prng):
+    regs = []
+    X = []
+    D = []
+    for l in range(1, 4): #Used to be 6
+        for x in range(2**(l+3)):
+            cols = prng.permutation(seed_db.columns[:-1])
+            db_groups = seed_db.groupby(list(cols[:l])).groups
+            idxs = db_groups[list(db_groups)[prng.randint(len(db_groups))]]
+            L = idxs.size
+            L = min(L, 5000)
+            L = prng.randint(0.7*L, L)
+            idxs = prng.choice(idxs, L)
+            data = DB(seed_db.loc[idxs, cols[l:]], seed_db.loc[idxs, seed_db.columns[-1]], None, None, epsilon=eps, depth=l)
+            regs.append({name: alg.error(data) for name, alg in tree_algs.items()})
+            X.append(DBMetas()(data))
+            D.append(data)
+    #Large DBs    
+    for x in range(16):
+        cols = seed_db.columns[:-1]
+        L = len(seed_db)
+        L = min(L, 5000)
+        L = prng.randint(0.7*L, L)
+        new_db = seed_db.sample(L, random_state=prng)
+        data = DB(new_db.loc[:, cols], new_db.loc[:, seed_db.columns[-1]], None, None, epsilon=eps, depth=0)
+        regs.append({name: alg.error(data) for name, alg in tree_algs.items()})
+        X.append(DBMetas()(data))
+        D.append(data)
+    return (regs, X, D)
+
+"""
+Does a similar thing as get_train_dbs, but makes fewer slices, and the slices are large
+because these are databases we want to test on (and we probably won't be using tiny dbs in real life).
+"""
+
+def get_test_dbs(seed_db, eps, prng):
+    cols = seed_db.columns[:-1]
+    y_col = seed_db.columns[-1]
+    L = len(seed_db)
+    L = min(L, int(5000/0.7))
+    L = prng.randint(0.7*L, L)
+    new_db = seed_db.sample(L, random_state=prng).reset_index(drop=True)
+    split = int(0.7*L)
+    md = min(len(cols), 4)
+    d = DB(new_db.loc[:split, cols], new_db.loc[:split, y_col], \
+           new_db.loc[split:, cols], new_db.loc[split:, y_col], epsilon=eps, max_depth=md)
+    return d
+
+
---
+++ Python/decision_tree_data/letter-recognition.py
@@ -0,0 +1,18 @@
+
+df = pd.read_csv('../data/letter-recognition.csv', header=None)
+df.apply(lambda c: c.astype('category'))
+df['TARGET'] = df[0]
+df = df.drop([0], axis=1)
+
+def sample_df(df, num_letters=6):
+    letters = [chr(x+65) for x in np.random.choice(26, num_letters, replace=False)]
+    rows = np.any([df.TARGET == l for l in letters], axis=0)
+    return df[rows]
+
+D5 = sample_df(df, 5)
+D6 = sample_df(df, 6)
+D7 = sample_df(df, 7)
+D8 = sample_df(df, 8)
+
+import pickle
+pickle.dump([D5, D6, D7, D8], open('letter.pkl', 'wb'))
