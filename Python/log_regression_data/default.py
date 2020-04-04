0e9c9a213b44271000466aac2956ae2ef3771764
Jacob Imola <jimolaaa@gmail.com>
2020-04-04 15:44:07 -0700

    Added different form of default dataset

--- Python/log_regression_data/default.py
+++ Python/log_regression_data/default.py
@@ -1,14 +1,6 @@
 #The code we use to preprocess the default dataset for log_regression_data.
-#This file must be run from within LogRegChoice.py
 
-default = pd.read_csv('data/application_train.csv')
-default['TARGET'] = le.fit_transform(default['TARGET'])
-#For simplicity, get rid of all columns with missing data
-default = default[default.columns[default.notnull().all()]].sample(10000)
+D = pd.read_csv('../data/default.csv')
+D = D.rename({'default payment next month': 'TARGET'}, axis=1)
 
-new_cols = list(default.columns)
-new_cols[1] = new_cols[-1]
-new_cols[-1] = 'TARGET'
-default = default[new_cols]
-
-pickle.dump(default, open('log_regression_data/default_preprocess.pkl', 'wb'))
+pickle.dump(D, open('default_preprocess.pkl', 'wb'))
